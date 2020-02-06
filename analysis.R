f954acf34a8ac3169119519e683ed9d3223fe870
Andrew Wilson <andrew.wilson849@icloud.com>
2020-02-05 17:15:23 -0800

    feat: Initializes analysis script

---
+++ analysis.R
@@ -0,0 +1,77 @@
+# Andrew Wilson
+# 14 November 2019
+# Song, Y. et al
+
+#########
+# Setup #
+#########
+
+# Load packages
+library(tidyverse)
+library(vegan)
+library(phyloseq)
+library(DESeq2)
+library(cowplot)
+
+# Set working directory
+setwd(".")
+# Load data
+hsm_otu_table <- read.table("otu_table.tsv", header = TRUE, sep = "\t")
+hsm_16s_metadata <- read.csv("metadata.csv")
+hsm_16s_taxonomy <- read.table("taxonomy.tsv", header = TRUE, sep = "\t")
+condition_of_interest <- c("BS", "HSM", "Col0", "W.Col0")
+#############
+# Diversity #
+#############
+
+# Rarefy OTUs for diversity measurements
+otu_names <- as.character(hsm_otu_table$OTU_ID)
+hsm_otu_table <- data.frame(t(hsm_otu_table[,-1]))
+colnames(hsm_otu_table) <- otu_names
+hsm_otus_rarified <- data.frame(rrarefy(hsm_otu_table, 11095))
+
+# Separate samples by soil type
+s17_samples <- as.character(hsm_16s_metadata$sample_name[hsm_16s_metadata$soil=="s17" & hsm_16s_metadata$condition %in% condition_of_interest])
+
+s17_rarified_otus <- filter(hsm_otus_rarified, rownames(hsm_otus_rarified) %in% s17_samples)
+hsm_otus_rarified <- s17_rarified_otus
+
+s17_unrarified_otus <- filter(hsm_otu_table, rownames(hsm_otu_table) %in% s17_samples)
+
+# Alpha Diversity measurements
+alpha_diversity <- data.frame("sample_name" = s17_samples, 
+                              "inverse_simpsons" = diversity(hsm_otus_rarified, "invsimpson", MARGIN = 1),
+                              "shannon" = diversity(hsm_otus_rarified, MARGIN = 1),
+                              "chao1" = estimateR(hsm_otus_rarified)[2, ])%>%
+  gather(key = "metric", value = "index", -1)%>%
+  inner_join(hsm_16s_metadata)
+
+
+alpha_diversity_plot <- ggplot(alpha_diversity, aes(condition, index, fill = soil))+
+  facet_wrap(.~ metric, scales = "free")+
+  geom_boxplot()
+
+alpha_diversity_plot
+
+# MDS of Bray-Curtis dissimilarity
+## Run MDS on S17 samples
+s17_mds <- data.frame(metaMDS(s17_rarified_otus)$points)
+s17_mds$sample_name <- s17_samples
+s17_mds <- inner_join(hsm_16s_metadata, s17_mds)
+## Plot S17 MDS
+s17_mds_plot <- ggplot(s17_mds, aes(MDS1, MDS2, colour = condition))+
+  geom_point()+
+  coord_fixed()
+s17_mds_plot
+
+## Run PERMANOVA for each MDS plot
+permanova_s17 <- adonis(s17_rarified_otus ~ condition, data = s17_mds)
+capture.output(permanova_s17, file="permanova.txt")
+
+
+# Family level abundances
+
+
+
+
+
